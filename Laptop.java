6b9f160c3887bde52df9ff4ddba8dcf322c58b7e
nasreen05 <reshmanasreen30@gmail.com>
2025-08-03 11:29:41 +0530

    twenty one commit

---
+++ Laptop.java
@@ -0,0 +1,44 @@
+ class Laptop{
+	
+	private String name;
+	private String marketAdd;
+	private int price;
+
+public void setName(String name){
+	this.name=name;
+}
+
+
+public String getName(){
+	return name;
+}
+public void setMarketAdd(String marketAdd){
+this.marketAdd=marketAdd;
+}
+
+public String getMarketAdd(){
+	return marketAdd;
+}
+public int price(){
+	return price;
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
