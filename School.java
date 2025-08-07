8b4bbad10f37c210bd8bcaa336cfd4a5f92d0c5c
nasreen05 <reshmanasreen30@gmail.com>
2025-08-07 09:00:57 +0530

    Add local files

--- School.java
+++ School.java
@@ -1,7 +1,11 @@
 class School {
+
+	
+
 	private String name;
 	private int schoolNumber;
 	private String schoolAddress;
+
 
 	int[]  numberofClassroom={101,102,103,104,105,};
 	int[] numberofClass={9,10,11,12};
@@ -29,4 +33,56 @@ class School {
     this.schoolAddress = schoolAddress;
 	}
 	}
+
+	private Teachers teachers;
+
+	public School(){
+	}
+
+	public School(String name, int schoolNumber){
+		System.out.println("Initialize name ,schoolNumber");
+		this.name = name; 
+		this.schoolNumber = schoolNumber;
+	}
+
+	public School(String name, int schoolNumber, String schoolAddress){ 
+		this(name, schoolNumber);
+		System.out.println("Initialize schoolAddress");
+		this.schoolAddress = schoolAddress; 
+	}
+
+	public void setteachers(Teachers teachers){
+		this.teachers = teachers;
+	}
+
+	public Teachers getteachers(){
+		return teachers;
+	}
+
+	public String getName(){
+		return this.name;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public int getSchoolNumber(){
+		return this.schoolNumber;
+	}
+
+	public void setSchoolNumber(int schoolNumber){
+		this.schoolNumber = schoolNumber;
+	}
+
+	public String getSchoolAddress(){
+		return this.schoolAddress;
+	}
+
+	public void setSchoolAddress(String schoolAddress){
+		this.schoolAddress = schoolAddress;
+	}
+}
+
+
 	
