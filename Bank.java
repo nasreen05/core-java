bf7ea3062b1b2e15089c30ac3cf3ce36ebe80462
nasreen05 <reshmanasreen30@gmail.com>
2025-08-20 22:48:29 +0530

    Initial commit: added full project folder

---
+++ Bank.java
@@ -0,0 +1,21 @@
+class Bank {
+    private String name;
+    private String marketAdd;
+    private long price;
+
+    public void setName(String name){
+     this.name=name;
+     }
+    public String getName(){ 
+        return name;
+     }
+    public void setMarketAdd(String marketAdd){ 
+    this.marketAdd=marketAdd;
+     }
+    public String getMarketAdd(){ 
+    return marketAdd;
+     }
+    public long price(){
+     return price;
+      }
+}
---
+++ name1.java
@@ -0,0 +1,5 @@
+public class name1{
+public static void main(String[]args){
+System.out.println("neha");
+}
+}
